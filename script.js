064873ae08e32eb0ede1d25b9dad1a07ff3ad2e7
inseofmi <ismailhiraev3@gmail.com>
2025-11-05 22:49:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const fadeElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+fadeElements.forEach(el => observer.observe(el));
